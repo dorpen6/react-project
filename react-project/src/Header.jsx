68e33160a44bec08b3cad728fefbb1dcda84654e
Ali Valishov <alivalishov20@gmail.com>
2023-12-17 18:57:57 +0100

    second commit

--- react-project/src/Header.jsx
+++ react-project/src/Header.jsx
@@ -2,7 +2,12 @@ function Header() {
     return(
         <header>
             <p>
-                Hello World
+                Hello World,
+                1
+                2
+                3
+                4
+                5
             </p>
         </header>
     );
