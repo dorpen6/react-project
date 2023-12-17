af9f8519d91240d9de71a852570d7091830eed48
Ali Valishov <alivalishov20@gmail.com>
2023-12-17 18:55:08 +0100

    first commit

---
+++ react-project/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            {console.log("Müəllif hüquqları DiexMedia tərəfindən qorunur")}<p>Müəllif hüquqları DiexMedia tərəfindən qorunur</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ react-project/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <header>
+            <p>
+                Hello World
+            </p>
+        </header>
+    );
+}
+
+export default Header
